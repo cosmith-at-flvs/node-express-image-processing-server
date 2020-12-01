65067bd617bcdfbb96935f5c3adb2232385490e6
Smith <cosmith@flvs.net>
2020-12-01 08:43:15 -0500

    check in

--- api/app.js
+++ api/app.js
@@ -1,10 +1,10 @@
-const express = require('express')
-const path = require('path')
+const express = require('express');
+const path = require('path');
 
-const app = express()
+const app = express();
 
-const pathToIndex = path.resolve(__dirname, '../client/index.html')
+const pathToIndex = path.resolve(__dirname, '../client/index.html');
 
-app.use('/*', (request, response) => response.sendFile(pathToIndex))
+app.use('/*', (request, response) => response.sendFile(pathToIndex));
 
-module.exports.app = 
+module.exports = app;
