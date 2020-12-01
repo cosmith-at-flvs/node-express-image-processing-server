b11b49a295fc5304eb0ba7101a78f8f5b45fc2d1
Smith <cosmith@flvs.net>
2020-12-01 10:52:54 -0500

    before test script

--- api/app.js
+++ api/app.js
@@ -5,9 +5,10 @@ const router = require('./src/router');
 const app = express();
 
 app.use('/', router);
-app.use(express.static(path.resolve(__dirname,'uploads')));
 
 const pathToIndex = path.resolve(__dirname, '../client/index.html');
 app.use('/*', (request, response) => {response.sendFile(pathToIndex);});
+
+app.use(express.static(path.resolve(__dirname,'uploads')));
 
 module.exports = app;
